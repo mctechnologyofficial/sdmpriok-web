e4cab5b86da455a877aa9d089f8772bb68da2050
Fawwaz Saputra <manusiacoding29@gmail.com>
2022-10-02 14:45:30 +0700

    add logo on login page view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,7 +12,7 @@
 		<link rel="icon" href="{{ asset('assets/img/brand/favicon.ico') }}" type="image/x-icon"/>
 
 		<!-- Title -->
-		<title>Spruha - Bootstrap Premium HTML Dashboard Template</title>
+		<title>Indonesia Power Dashboard - Login</title>
 
 		<!-- Bootstrap css-->
 		<link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"/>
@@ -51,22 +51,22 @@
 						<div class="row row-sm">
 							<div class="col-lg-6 col-xl-5 d-none d-lg-block text-center bg-primary details">
 								<div class="mt-5 pt-4 p-2 pos-absolute">
-									<img src="{{ asset('assets/img/brand/logo-light.png') }}" class="header-brand-img mb-4" alt="logo">
+									<img src="{{ asset('assets/img/brand/logo-ip.png') }}" class="header-brand-img mb-4 w-75" alt="logo">
 									<div class="clearfix"></div>
 									<img src="{{ asset('assets/img/svgs/user.svg') }}" class="ht-100 mb-0" alt="user">
-									<h5 class="mt-4 text-white">Create Your Account</h5>
-									<span class="tx-white-6 tx-13 mb-5 mt-xl-0">Signup to create, discover and connect with the global community</span>
+									<h5 class="mt-4 text-white">Aplikasi SDM Indonesia Power</h5>
+									{{-- <span class="tx-white-6 tx-13 mb-5 mt-xl-0">Signup to create, discover and connect with the global community</span> --}}
 								</div>
 							</div>
 							<div class="col-lg-6 col-xl-7 col-xs-12 col-sm-12 login_form ">
 								<div class="container-fluid">
 									<div class="row row-sm">
 										<div class="card-body mt-2 mb-2">
-											<img src="{{ asset('assets/img/brand/logo.png') }}" class=" d-lg-none header-brand-img text-left float-left mb-4" alt="logo">
+											<img src="{{ asset('assets/img/brand/logo-ip.png') }}" class="w-50 d-lg-none header-brand-img text-left float-left mb-4" alt="logo">
 											<div class="clearfix"></div>
 											<form>
 												<h5 class="text-left mb-2">Signin to Your Account</h5>
-												<p class="mb-4 text-muted tx-13 ml-0 text-left">Signin to create, discover and connect with the global community</p>
+												{{-- <p class="mb-4 text-muted tx-13 ml-0 text-left">Signin to create, discover and connect with the global community</p> --}}
 												<div class="form-group text-left">
 													<label>Email</label>
 													<input class="form-control" placeholder="Enter your email" type="text">
@@ -77,9 +77,9 @@
 												</div>
 												<button class="btn ripple btn-main-primary btn-block">Sign In</button>
 											</form>
-											<div class="text-left mt-5 ml-0">
+											<div class="text-left mt-3 ml-0">
 												<div class="mb-1"><a href="">Forgot password?</a></div>
-												<div>Don't have an account? <a href="#">Register Here</a></div>
+												{{-- <div>Don't have an account? <a href="#">Register Here</a></div> --}}
 											</div>
 										</div>
 									</div>
