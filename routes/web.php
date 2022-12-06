0f73d01f5717c52a43085e4dd29f4c1cc38bcbac
ciwanridwan <ridwan.andalus79@gmail.com>
2022-12-06 23:14:57 +0700

    fix revert route to dinamis roles

--- routes/web.php
+++ routes/web.php
@@ -217,8 +217,7 @@ Route::group(['middleware' => ['role:supervisor']], function () {
     });
 });
 
-Route::group(['middleware' => ['role:Operator GT RSG|Supervisor Operator|Senior Operator|Ahli Muda Operator|Operator Senior Control Room']], function () {
-    // Route::group(['middleware' => ['role:'. Role::getOtherRolesByName()]], function () {
+    Route::group(['middleware' => ['role:'. Role::getOtherRolesByName()]], function () {
     // operator routes
     Route::prefix('operator')->group(function () {
         Route::get('/home', [HomeController::class, 'IndexOperator'])->name('op.index');
